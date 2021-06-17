d90aa925cf77700f01019a3f843d9cc4b8df112b
Ivan Čurić <ivan.curic@gmail.com>
2021-06-17 15:45:29 +0200

    cleanup unnecessary modules

--- lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  '*.{js,jsx,ts,tsx}': ['eslint --cache --fix --max-warnings=0'],
-  '*.{ts,tsx}': [() => 'tsc --skipLibCheck --noEmit'],
-  '*.{js,jsx,ts,tsx,css,md}': ['prettier --write'],
-};

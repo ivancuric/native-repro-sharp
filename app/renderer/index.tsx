fb0d346661032d6f2b92877c285c167b1d0516bb
Ivan Čurić <ivan.curic@gmail.com>
2021-06-23 12:21:40 +0200

    Sharp works!

--- app/renderer/index.tsx
+++ app/renderer/index.tsx
@@ -1,18 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import 'react-hot-loader';
 
+import helloWorld from 'native-hello-world';
 import ReactDOM from 'react-dom';
 import sharp from 'sharp';
 import videoModule from 'video-module';
 
 import App from '~/components/App';
 
-// @ts-ignore
-// window.sharp = sharp;
-// @ts-ignore
-window.videoModule = videoModule;
-
 console.log(sharp);
+helloWorld();
 console.log(videoModule);
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.externals.js
+++ webpack.externals.js
@@ -1,4 +1,5 @@
 module.exports = {
   sharp: 'commonjs2 sharp',
+  'native-hello-world': 'commonjs2 native-hello-world',
   'video-module': 'commonjs2 video-module',
 };
